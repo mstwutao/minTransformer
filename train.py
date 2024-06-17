46d61b6d1cefea3b84067868fc3567b5c39c321f
mstwutao <wuta@mst.edu>
2024-06-16 23:02:17 -0500

    update train

--- train.py
+++ train.py
@@ -11,10 +11,9 @@ if __name__ == '__main__':
     transformer.train()
 
     loss_fn = nn.CrossEntropyLoss(ignore_index=Config.PAD_IDX)
-    optimizer = torch.optim.Adam(transformer.parameters(), lr=1e-3)
+    optimizer = torch.optim.Adam(transformer.parameters(), lr=1e-4)
 
     for epoch in range(Config.epochs):
-        batch_i = 0
         loss_sum = 0
         for pad_enc_input, pad_dec_input in dataloader:
             pad_enc_input = pad_enc_input.to(Config.device)
@@ -26,10 +25,8 @@ if __name__ == '__main__':
                                             dec_pred.size()[-1]),
                            dec_input_gt.reshape(-1))
             loss_sum += loss.item()
-            batch_i += 1
-            print(f'epoch:{epoch} batch:{batch_i} loss:{loss.item()}')
-
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
+        print(f'epoch:{epoch}, loss:{loss.item()}')
         torch.save(transformer, f'epoch_{epoch}.pth')
